aa23182f6a46c3f0d4f1a0beb41a0c01bb3fcd75
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-28 22:02:05 -0800

    Add details to be shown

---
+++ homeowner-helper-proj/src/components/TaskDetail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//Need importstatement for LogList
+
+function TaskDetail(props) {
+
+  return (
+    <React.Fragment>
+      area
+      avg time to complete
+      MaterialList
+      Frequency
+      About/Tips
+      Next Due
+      <LogList />
+    </React.Fragment>
+  )
+}
+
+export default TaskDetail;
