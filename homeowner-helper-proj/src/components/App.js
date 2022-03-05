a7bf19d059794be96175a42dffa20c25eb3eb70e
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-04 18:35:34 -0800

    Add components, provider

--- homeowner-helper-proj/src/components/App.js
+++ homeowner-helper-proj/src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import TaskControl from './TaskControl';
+import TaskList from './TaskList';
+import TaskContextProvider from '../contexts/TaskContext';
 
 function App() {
   return (
-    
+    <div className="App">
+      <TaskContextProvider>
+        <TaskList />
+      </TaskContextProvider>
+    </div>
   );
 }
 
