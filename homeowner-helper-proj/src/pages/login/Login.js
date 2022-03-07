176910ed1ee27d0c3146774f0d09277db86360c7
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 18:55:36 -0800

    implement useLogin

--- homeowner-helper-proj/src/pages/login/Login.js
+++ homeowner-helper-proj/src/pages/login/Login.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 
 //styles
 import styles from './Login.module.css';
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isPending, error } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password)
+    login(email, password)
   }
 
   return (
@@ -31,7 +33,9 @@ export default function Login() {
           value={password}
         />
       </label>
-      <button className="btn">Login</button>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>...loading</button>}
+      {error && <p>{error}</p>}
     </form>
   )
 }
