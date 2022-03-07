a6a9e6046bc897aee3d923a196b37ed10cdaef93
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 16:12:26 -0800

    Add useSignup w/ error and loading functionality

--- homeowner-helper-proj/src/pages/signup/Signup.js
+++ homeowner-helper-proj/src/pages/signup/Signup.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
+  const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName)
+    signup(email, password, displayName)
   }
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
@@ -35,7 +37,9 @@ export default function Signup() {
           value={password}
         />
       </label>
-      <button className="btn">Signup</button>
+      {!isPending && <button className="btn">Signup</button>}
+      {isPending && <button>...loading</button>}
+      {error && <p>{error}</p>}
     </form>
   )
 }
