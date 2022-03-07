4520470cfda9ba057d20ae69e1420561e5511a82
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 19:39:16 -0800

    Add Home and Redirects

--- homeowner-helper-proj/src/components/App.js
+++ homeowner-helper-proj/src/components/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { useAuthContext } from '../hooks/useAuthContext'
 // import TaskControl from './TaskControl';
 // import TaskList from './TaskList';
 // import TaskContextProvider from '../context/TaskContext';
 
 // pages and components
-// import Home from '../pages/home/Home'
+import Home from '../pages/home/Home'
 import Signup from '../pages/signup/Signup';
 import Login from '../pages/login/Login';
 import Navbar from './Navbar';
 
 
 function App() {
-  const { authIsReady } = useAuthContext()
+  const { authIsReady, user } = useAuthContext()
   return (
     <div className="App">
       {authIsReady && (
@@ -21,13 +21,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              Home
+              {!user && <Redirect to="/login" />}
+              { user && <Home />}
             </Route>
             <Route path="/login">
-              <Login />
+              {!user && <Login />}
+              {user && <Redirect to="/" />}
             </Route>
             <Route path="/signup">
-              <Signup />
+              {!user && <Signup />}
+              {user && <Redirect to="/" />}
             </Route>
           </Switch>
         </BrowserRouter>
