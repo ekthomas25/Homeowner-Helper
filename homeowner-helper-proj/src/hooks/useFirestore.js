db89e3214774bb66750531b13abf6fc262acf538
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-07 14:34:02 -0800

    BUGFIX: add (err) to catch

--- homeowner-helper-proj/src/hooks/useFirestore.js
+++ homeowner-helper-proj/src/hooks/useFirestore.js
@@ -1,57 +1,57 @@
-import { useReducer, useEffect, useState } from 'react';
-import { projectAuth, timestamp } from '../firebase/config';
+import { useReducer, useEffect, useState } from "react"
+import { projectFirestore, timestamp } from "../firebase/config"
 
 let initialState = {
   document: null,
   isPending: false,
   error: null,
-  success: null
+  success: null,
 }
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-    case 'IS_PENDING':
-      return { error: null, isPending: true, document: null, success: null }
-    case 'ADDED_DOCUMENT':
-      return { error: null, isPending: false, document: action.payload, success: true }
-    case 'ERROR':
-      return { error: action.payload, isPending: false, document: null, success: false }
+    case "IS_PENDING":
+      return {success: false, isPending: true, error: null, document: null}
+    case "ERROR":
+      return {success: false, isPending: false, error: action.payload, document: null}
+    case "ADDED_DOCUMENT":
+      return {success: true, isPending: false, error: null, document: action.payload}
     default:
       return state
   }
 }
 
 export const useFirestore = (collection) => {
-  const [response, dispatch] = useReducer(firestoreReducer, initialState);
-  const [isCancelled, setIsCancelled] = useState(false);
-  
+  const [response, dispatch] = useReducer(firestoreReducer, initialState)
+  const [isCancelled, setIsCancelled] = useState(false)
 
-  // collection reference
-  const ref = projectFirestore.collection(collection);
+  // collection ref
+  const ref = projectFirestore.collection(collection)
 
-  //only dispatch if not cancelled
-  const dispatchIfNotCancelled  = (action) => {
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
     if (!isCancelled) {
       dispatch(action)
     }
   }
-
+  
   // add a document
   const addDocument = async (doc) => {
-    dispatch({ type: 'IS_PENDING' });
+    dispatch({ type: "IS_PENDING" })
 
     try {
-      const createdAt = timestamp.fromDate(new Date());
-      const addedDocument = await ref.add(...doc, createdAt);
-      dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument });
+      const createdAt = timestamp.fromDate(new Date())
+      const addedDocument = await ref.add({...doc, createdAt })
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument })
     }
-    catch {
-      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message });
+    catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
     }
+
   }
 
   // delete a document
-  const deleteDocument = async (id) => {
+  const deleteDocument = async (doc) => {
 
   }
 
@@ -60,4 +60,5 @@ export const useFirestore = (collection) => {
   }, [])
 
   return { addDocument, deleteDocument, response }
+
 }
