e7ab927e55e8a85d14b8aa17a278bb3db5ae66de
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 18:34:23 -0800

    Add logout button

--- homeowner-helper-proj/src/components/Navbar.js
+++ homeowner-helper-proj/src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 //styles
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
+  const { logout } = useLogout();
   return (
     <nav className={styles.navbar}>
       <ul>
-      <li><Link to="/">Home Helper</Link></li>
-      <li><Link to="/login">Login</Link></li>
-      <li><Link to="/signup">Signup</Link></li>
+        <li className={styles.title}><Link to="/">Home Helper</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+
+        <li>
+          <button className="btn" onClick={logout}>Logout</button>
+        </li>
       </ul>
     </nav>
   );
