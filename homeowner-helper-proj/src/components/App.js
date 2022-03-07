4b83da3e8bb293245ff9ff0df7741cc197f42506
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 16:42:25 -0800

    Add BrowserRouter, Navbar, Switch, Routes

--- homeowner-helper-proj/src/components/App.js
+++ homeowner-helper-proj/src/components/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import TaskControl from './TaskControl';
-import TaskList from './TaskList';
-import TaskContextProvider from '../contexts/TaskContext';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// import TaskControl from './TaskControl';
+// import TaskList from './TaskList';
+// import TaskContextProvider from '../context/TaskContext';
+
+// pages and components
+// import Home from '../pages/home/Home'
+import Signup from '../pages/signup/Signup';
+import Login from '../pages/login/Login';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <TaskContextProvider>
-        <TaskList />
-      </TaskContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route path="/login">
+            Login
+          </Route>
+          <Route path="/signup">
+            Signup
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
