92c65dfeb01076869a97eeaada3d9ec480b2409f
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-06 17:30:22 -0800

    Add logout, state

--- homeowner-helper-proj/src/hooks/useLogout.js
+++ homeowner-helper-proj/src/hooks/useLogout.js
@@ -2,6 +2,31 @@ import React, { useState } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
-const useLogout = () => {
-  
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    //sign user out
+    try {
+      await projectAuth.signOut()
+
+      //dispatch logout action
+      dispatch({ type: 'LOGOUT' })
+
+      setisPending(false)
+      setError(null)
+    }
+    catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  }
+
+  return { logout, isPending, error }
 }
