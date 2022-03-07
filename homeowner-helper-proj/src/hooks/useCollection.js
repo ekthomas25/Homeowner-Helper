126e24395e5471c1e96848fbc3217d03c668a782
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-07 14:57:52 -0800

    create hook to update realtime subscription

---
+++ homeowner-helper-proj/src/hooks/useCollection.js
@@ -0,0 +1,30 @@
+import {useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config'
+
+export const useCollections = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+
+    const unsub = ref.onSnapshot((snapshot) => {
+      let results = [];
+      snapshot.docs.forEach(doc => {
+        results.push({ ...doc.data(), id: doc.id })
+      })
+
+      //update state withs
+      setDocuments(results);
+      setError(null)
+    }, (error) => {
+      console.log(error)
+      setError('Coud not fetch data')
+    })
+
+    //unsubscribe on unmount
+    return () => unsub()
+  }, [collection])
+
+  return { documents, error }
+}
