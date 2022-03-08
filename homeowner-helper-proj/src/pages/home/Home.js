7c8cd87158d00dd2ddfe0ff9aefcdae7f81007ed
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-08 08:46:10 -0800

    Add styles, classname, error

--- homeowner-helper-proj/src/pages/home/Home.js
+++ homeowner-helper-proj/src/pages/home/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import TaskList from '../../components/TaskList';
 
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useCollection } from '../../hooks/useCollection'
+
+//styles
+import './Home.css'
 
 export default function Home() {
   const { user } = useAuthContext()
   const { documents, error } = useCollection('tasks')
   return (
-    <>
+    <div className="home">
       <h1>Home</h1>
-      
-    </>
+      {error && <p>{error}</p>}
+      <TaskList />
+    </div>
   )
 }
