faa9f9b16f440bbc7caf3d340d96a50240006dbe
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-07 20:34:50 -0800

    remove TaskForm

--- homeowner-helper-proj/src/pages/home/Home.js
+++ homeowner-helper-proj/src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TaskForm from './TaskForm';
+
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useCollection } from '../../hooks/useCollection'
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <TaskForm uid={user.uid} />
+      
     </>
   )
 }
