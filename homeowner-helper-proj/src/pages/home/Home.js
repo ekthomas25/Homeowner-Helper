eb3c32e0fc86684e472a494f78d1c66341f6ba25
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-07 17:02:03 -0800

    Add useCollection

--- homeowner-helper-proj/src/pages/home/Home.js
+++ homeowner-helper-proj/src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TaskForm from './TaskForm';
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection'
 
 export default function Home() {
   const { user } = useAuthContext()
+  const { documents, error } = useCollection('tasks')
   return (
     <>
       <h1>Home</h1>
