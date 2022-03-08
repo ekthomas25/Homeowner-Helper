13e2c1d31ccac0e1c82c8a916f545d46d8cfd63b
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-07 22:02:41 -0800

    remove context

--- homeowner-helper-proj/src/components/TaskList.js
+++ homeowner-helper-proj/src/components/TaskList.js
@@ -1,19 +1,18 @@
-import React, { useContext } from 'react';
-import TaskDetail from './TaskDetail';
-import { TaskContext } from '../contexts/TaskContext';
+import React from 'react';
+import { useCollection } from '../hooks/useCollection';
+
+//styles
+import './TaskList.css'
 
 const TaskList = () => {
-  const { tasks } = useContext(TaskContext);
-  return tasks.length ? (
+  const { documents } = useCollection('tasks');
+  return (
     <div className="task-list">
-      <ul>
-        {tasks.map(task => {
-          return( <TaskDetail task={task} key={task.id} />)
-        })}
-      </ul>
+      {!documents && <p>No Tasks</p>}
+      {documents && documents.map(doc => (
+        <div className="task-item" key={doc.id}>{doc.taskName}</div>
+      ))}
     </div>
-  ) : (
-    <div className="empty">No books to read. Hello free time :D</div>
   );
 }
 
