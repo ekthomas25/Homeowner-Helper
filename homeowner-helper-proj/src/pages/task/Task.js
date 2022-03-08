bb3325b2a232c913692bfe12a07b9f3478f5a3a2
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-08 13:12:49 -0800

    Add useDocument, useParams, check for error and document

--- homeowner-helper-proj/src/pages/task/Task.js
+++ homeowner-helper-proj/src/pages/task/Task.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useDocument } from '../../hooks/useDocument';
+import { useParams } from 'react-router-dom';
 
 export default function Task() {
+  const { id } = useParams();
+  const {error, document } = useDocument('tasks', id);
+
+  if(error) {
+    return <div className="error">{error}</div>
+  }
+
+  if (!document) {
+    return <div className="loading">loading...</div>
+  }
+
   return (
-    <React.Fragment>
-      taskName - nextDue
-    </React.Fragment>
+    <div className="task-details">
+      <h1>{document.taskName}</h1>
+    </div>
     
   )
 }
