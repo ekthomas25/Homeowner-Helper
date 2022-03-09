36e2c4654b896648c892984da0b9c12c6420fddc
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-08 17:35:04 -0800

    Add useState hook, handleShowLogForm, handleCancleLogForm, buttons

--- homeowner-helper-proj/src/pages/task/TaskDetail.js
+++ homeowner-helper-proj/src/pages/task/TaskDetail.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import LogForm from '../../components/LogForm'
 import LogList from '../../components/LogList';
 
 export default function TaskDetail({ task }) {
+  const [showLogForm, setShowLogForm] = useState(false)
   console.log(task)
+
+  const handleShowLogForm = () => {
+    setShowLogForm(true)
+    console.log(showLogForm);
+  }
+
+  const handleCancelLogForm = () => {
+    setShowLogForm(false)
+  }
+
   return (
+    <>
     <div className="task-detail-view">
       <div className="task-summary">
         <span>Next Due: </span>{task.nextDue.toDate().toDateString()}
@@ -24,8 +36,10 @@ export default function TaskDetail({ task }) {
           {task.materialList.map(materialList => <li key={materialList}>{materialList}</li>)}
         </ul>
       </div>
+      <button className="btn" onClick={handleShowLogForm}>Complete Task</button>
     </div>
+      {showLogForm && <LogForm task={task} handleCancelLogForm={handleCancelLogForm}/>}
       
-    
+      </>
   )
 }
