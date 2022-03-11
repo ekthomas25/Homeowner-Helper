bfbfa7fa5d8d868a5be850a73a720efb523f45b3
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-10 17:42:59 -0800

    Add hooks, cost, and date

--- homeowner-helper-proj/src/pages/dashboard/Dashboard.js
+++ homeowner-helper-proj/src/pages/dashboard/Dashboard.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useParams } from 'react';
 import NextDue from './NextDue'
 import { timestamp } from '../../firebase/config'
-
+import { useDocument } from '../../hooks/useDocument';
 import { useCollection } from '../../hooks/useCollection'
 import PastDue from './PastDue';
+import Cost from './Cost';
 
 //styles
 import './Dashboard.css'
 
 export default function Dashboard() {
-  const { id } = useParams();
-  const {error, document } = useDocument('tasks', id);
-  
+  // const { id } = useParams();
+  // const {error, document } = useDocument('tasks', id);
+  const { documents, error } = useCollection('tasks')
   // console.log({documents})
   console.log(Date.now()/1000)
   console.log(timestamp.fromDate(new Date()).seconds)
@@ -29,11 +30,16 @@ export default function Dashboard() {
   
   return (
     <div className="home">
-      <h2>Today's Date: {new Date().toDateString()}</h2>
+      <h1>Dashboard</h1>
+      <div className="past-due">
+      <h2>Today's Date:</h2>
+      {new Date().toDateString()}
+      </div>
       <PastDue />
       <div className="blorp">
         <NextDue />
       </div>
+      <Cost tasks={documents}/>
       {/* {dates} */}
       {/* <NextDue tasks={documents} /> */}
       {/* <h1>Your Tasks:</h1>
