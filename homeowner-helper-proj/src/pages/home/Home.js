d1352bf1e27d3d0adc93a7ab997c6fe59d03cc6a
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-10 17:44:08 -0800

    change h1 text, max height

--- homeowner-helper-proj/src/pages/home/Home.js
+++ homeowner-helper-proj/src/pages/home/Home.js
@@ -39,7 +39,7 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1>Your Tasks:</h1>
       {documents && <TaskFilter 
         changeFilter={changeFilter}
         currentFilter={filter} 
