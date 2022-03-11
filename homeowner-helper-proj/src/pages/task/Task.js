243188a034de14c375b91308537c6acc967b618e
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-10 17:48:37 -0800

    Add hooks, handleClick, divs

--- homeowner-helper-proj/src/pages/task/Task.js
+++ homeowner-helper-proj/src/pages/task/Task.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useDocument } from '../../hooks/useDocument';
 import { useParams } from 'react-router-dom';
 import TaskDetail from './TaskDetail';
+import LogList from '../../components/LogList';
 
+import { useCollection } from '../../hooks/useCollection'
 //styles
 import './Task.css';
 
@@ -18,10 +20,18 @@ export default function Task() {
     return <div className="loading">loading...</div>
   }
 
+  // const handleLogClick = (id) => {
+  //   setLogDetail((task.logs).filter(log => log.id === id));
+  //   setShowLogDetail(true)
+  // }
+  console.log(document.logs[0])
   return (
-    <div className="task-details">
+    <div className="home">
       <h1>{document.taskName}</h1>
-      <TaskDetail task={document} />
+      <div className="task-details">
+        <TaskDetail task={document} />
+        
+      </div>
     </div>
     
   )
