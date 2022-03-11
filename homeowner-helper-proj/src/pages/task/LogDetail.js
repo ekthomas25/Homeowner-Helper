942df9a9517247f1f216f0b68504a2e3fb8c6a64
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-10 17:46:21 -0800

    Add map, hooks, console.logs

--- homeowner-helper-proj/src/pages/task/LogDetail.js
+++ homeowner-helper-proj/src/pages/task/LogDetail.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { timestamp } from '../../firebase/config';
 import { useDocument } from '../../hooks/useDocument';
+import { useCollection } from '../../hooks/useCollection';
 import { useParams } from 'react-router-dom';
 
-export default function LogDetail({ task }) {
+export default function LogDetail({log}) {
   
   console.log("hey gurl")
-  // const { logs, id } = useParams();
-  // const {error, document } = useDocument('tasks', id, logs);
+  const { logs, id } = useParams();
+  const {error, document } = useDocument('logs', id);
+  const { documents  } = useCollection('tasks')
   // console.log(document)
   // if(error) {
   //   return <div className="error">{error}</div>
@@ -16,12 +18,13 @@ export default function LogDetail({ task }) {
   // if (!task) {
   //   return <div className="loading">loading...</div>
   // }
-
-  console.log(document.tasks.logs)
+  // console.log(document.logs)
+  console.log(documents, id)
+  console.log(document)
   return  (
     <div className="log-detail">
-      
-      <p>{task.logs.cost}</p>
+      <h2>Log Details</h2>
+      {documents.map(doc => (<p key={doc}>{doc}</p>))}
     </div>
   )
 }
