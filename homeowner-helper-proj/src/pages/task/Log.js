52428183413c98484ba2445a399c9af2e33f6752
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-05-18 22:06:42 -0700

    Add base function component

---
+++ homeowner-helper-proj/src/pages/task/Log.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { timestamp } from '../../firebase/config';
+
+export default function Log() {
+  
+  return  (
+    <div className="log">
+      
+    </div>
+  )
+}
